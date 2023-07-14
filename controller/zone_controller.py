b1a5b5bdd5d88aa9810f9149303335e3f1eb1afb
suzyido7 <138656087+suzyido7@users.noreply.github.com>
2023-07-14 12:09:55 +0300

    Update zone_controller.py

--- controller/zone_controller.py
+++ controller/zone_controller.py
@@ -1,27 +1,35 @@
 import uuid
 import os
 import json
-from flask import request, jsonify
+from flask import request
 from csv import writer, reader, DictReader, DictWriter
 from config import filename, tempfilename, fields
+from pathlib import Path
+
+def create_zones_file_if_not_exists():
+    if not Path(filename).is_file():
+        open(filename, "w").close()
+
+def transform_zone(zone):
+    return {fields[0]: zone[0], fields[1]: zone[1], fields[2]: json.loads(zone[2])}
 
 def create_zone():
+    create_zones_file_if_not_exists()
     zone_id = uuid.uuid1()
     with open(filename, 'a', newline='') as f_object:
         writer_object = writer(f_object)
         writer_object.writerow([zone_id, request.json[fields[1]], request.json[fields[2]]])
     return {"id": str(zone_id)}
 
-def transform_zone(zone):
-    return {fields[0]: zone[0], fields[1]: zone[1], fields[2]: json.loads(zone[2])}
-
 def get_zones():
+    create_zones_file_if_not_exists()
     with open(filename, newline='') as csvfile:
         zones = list(reader(csvfile))
 
     return [transform_zone(item) for item in zones]
 
 def delete_zone(zone):
+    create_zones_file_if_not_exists()
     is_zone_found = False
     print(zone)
     with open(filename, 'r') as csvfile, open(tempfilename, 'a', newline='') as tempfile:
